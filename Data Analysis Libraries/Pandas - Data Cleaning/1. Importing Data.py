6a9732a80ae57d129f11ed4ecc17ea21753a531c
Gary Chen <122473830+Gary-XC@users.noreply.github.com>
2024-03-19 10:59:46 -0400

    Starting Pandas Video, Notes, and practive problems

--- Data Analysis Libraries/Pandas - Data Cleaning/1. Importing Data.py
+++ Data Analysis Libraries/Pandas - Data Cleaning/1. Importing Data.py
@@ -1,4 +1,5 @@
 # https://www.youtube.com/playlist?list=PLUaB-1hjhk8GZOuylZqLz-Qt9RIdZZMBE
+# https://www.youtube.com/watch?v=CmorAWRsCAw&list=PLeo1K3hjS3uuASpe-1LjfG5f14Bnozjwy --> this is an older video, and some methods might be outdated, but the concepts and practice problems are still relevant
 
 import pandas as pd
 
