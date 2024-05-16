d374fc943f0f07aa97eca47c280e3e67d56a7f3b
Gary-XC <xichen.2460@gmail.com>
2024-05-16 16:09:18 -0400

    Connected to MySQL server, started different styles in Matplotlib and started c++ files

--- C++/1. C++ Intro.cpp
+++ C++/1. C++ Intro.cpp
@@ -1,5 +1,3 @@
-// drawing a shape
-
 #include <iostream>
 
 using namespace std;
@@ -8,6 +6,22 @@ int main()
 {
 
     cout <<"Hello World"<< endl;
+    cout <<"hello there would you like a bannana?"<< endl;
+    // simple c++ program for printing out hello world and strings/phrases to the console
+
+    cout << "     /|" << endl;
+    cout << "    / |" << endl;
+    cout << "   /  |" << endl;
+    cout << "  /___|" << endl;
+
+    // using cout and spaces in order to print a triangle to the console
+
+
+    /*
+    Variables
+    */
+
+
     return 0;
 
 }
