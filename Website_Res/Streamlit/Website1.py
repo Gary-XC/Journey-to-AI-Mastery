a142a49d5b93fac44d91e3a4366dde9038129ee0
Gary Chen <xichen.2460@gmail.com>
2025-01-24 21:51:52 -0500

    editing and adding new lessons

---
+++ Website_Res/Streamlit/Website1.py
@@ -0,0 +1,19 @@
+# https://readmedium.com/en/https:/medium.com/data-science-in-your-pocket/building-portfolio-using-streamlit-ac215b8e74da
+
+import streamlit as st
+import streamlit.components.v1 as components
+from PIL import Image
+
+
+
+st.sidebar.caption('Wish to connect?')
+
+st.sidebar.write('📧: xichen.2460@gmail.com')
+
+#pdfFileObj = open('Gary Chen Resume DS.pdf', 'rb') # might have to use the full file path
+
+#st.sidebar.download_button('text_to_display',pdfFileObj,file_name='Gary Chen Resume DS.pdf',mime='pdf')
+
+
+
+
